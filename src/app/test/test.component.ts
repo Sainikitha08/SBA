9dedce9c18d47eed607d1727334aef0018122c55
Sai Nikitha <c.sainikitha@dxc.com>
2023-07-04 15:13:05 +0530

    initial commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+  constructor(private myservice: MyServiceService){}
+  callServiceMethod(): void{
+    this.myservice.toDo("Hi this is Nikitha");
+  }
+
+}
